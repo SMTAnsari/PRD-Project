af1fe09656908e4ad55e5013371327447b885910
SMTAnsari <76551990+SMTAnsari@users.noreply.github.com>
2023-07-11 15:08:24 +0530

    Update Profile.js
    update

--- django-react/reactapp/src/Profile.js
+++ django-react/reactapp/src/Profile.js
@@ -11,7 +11,6 @@ const Profile = () => {
   const [dialogs, setdialogs] = useState(false);
   const [imgCrop, setimgCrop] = useState(null);
   const [storeImage, setstoreImage] = useState([]);
- 
 
   const onCrop = (view) => {
     setimgCrop(view);
@@ -22,32 +21,41 @@ const Profile = () => {
   };
 
   const saveImage = async () => {
-    try {
-      
 
-      const formData = {
-         profile_image: imgCrop.blob
-      };
-  console.log(formData.profile_image)
-      const response = await axios.post('http://127.0.0.1:8000/api/update-profile-image/', formData);
-      if (response.status >= 200) {
-        setstoreImage([{ imgCrop }]);
-        setdialogs(false);
-      } else {
-        console.log("Server Error")
+      if (imgCrop) {
+
+        fetch(imgCrop)
+          .then(res => res.blob())
+          .then(blob => {
+            const file = new File([blob], 'cropped-image.png', { type: blob.type });
+
+            const formData = new FormData();
+            formData.append('profile_image', file);
+            axios.post('http://127.0.0.1:8000/api/update-profile-image/', formData).
+              then(res => {
+                setstoreImage([{ imgCrop }]);
+                setdialogs(false);
+              })
+              .catch(error => {
+                console.log(error);
+              });
+          })
+          .catch(error => {
+            console.log('Error converting data URL to file:', error);
+          });
       }
-    } catch (error) {
-      console.log(error)
-    }
-  };
   
+   
+  };
+
 
   const profileImageShow = storeImage.map((item) => item.imgCrop);
-
+  
   return (
     <div className='main2'>
       <div className='submain'>
-      <Dialog className='dialog'
+        <Dialog
+          className='dialog'
           visible={dialogs}
           header={() => (
             <p className='text-2x1 front-semibold textColor'>
@@ -59,14 +67,13 @@ const Profile = () => {
           <div className="img1">
             <div className="img2">
               <div className="img3">
-                <Avatar className="avatar" width={400} height={300} onClose={onClose} onCrop={onCrop} />
-                <Button class="button-27" onClick={saveImage} label="Save" icon="pi pi-check" />
+              <Avatar width={400} height={300} onClose={onClose} onCrop={onCrop} />
+                <Button className = 'button-27' onClick={saveImage} label="Save" icon="pi pi-check" />
               </div>
             </div>
           </div>
         </Dialog>
-        
-        
+
         <img
           style={{
             width: "200px",
@@ -79,9 +86,6 @@ const Profile = () => {
           alt=''
           onClick={() => setdialogs(true)}
         />
-        
-
-       
       </div>
     </div>
   );
@@ -91,8 +95,8 @@ const App = () => {
   return (
     <div className='main'>
       <h1 className='heading' style={{
-            color: "initial",
-          }}>Profile Page</h1>
+        color: "initial",
+      }}>Profile Page</h1>
       <Profile />
     </div>
   );
